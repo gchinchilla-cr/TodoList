0377f612ccc631c727ed2e4a6e4844f94a264925
gchinchilla-cr <gchinchilla@live.com>
2021-03-12 04:30:29 +0000

    Proyecto TodoList

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,47 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import { Button } from "bootstrap";
+import { useEffect } from "react/cjs/react.production.min";
 
 //create your first component
 export function Home() {
+	const [lista, setLista] = useState([]);
+	const [tarea, setTarea] = useState("");
+	const [contador, setContador] = useState(0);
+	const agregar = () => {
+		if (tarea == "") {
+			alert("Digite la tarea que desea ingresar");
+		} else {
+			setLista([...lista, tarea]);
+			//alert("Su tarea ha sido ingresada");
+			setTarea("");
+			setContador(contador + 1);
+		}
+	};
+	const eliminar = i => {
+		const nuevaTarea = [...lista];
+		nuevaTarea.splice(i, 1);
+		setLista(nuevaTarea);
+		setContador(contador - 1);
+	};
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input
+				placeholder="Ingrese su tarea"
+				onChange={e => setTarea(e.target.value)}
+				value={tarea}></input>
+			<button onClick={agregar}>Agregar tarea</button>
+
+			{lista.map((element, i) => {
+				return (
+					<li key={i}>
+						{element}
+						<button className="remove" onClick={() => eliminar(i)}>
+							X
+						</button>
+					</li>
+				);
+			})}
+			<div>{contador + "  Tareas por hacer"}</div>
 		</div>
 	);
 }
